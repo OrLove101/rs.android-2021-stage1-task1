6a36601d8019a021d2b1c0c9bb4901f44772a74f
OrLove101 <danyazerorebel@gmail.com>
2021-05-21 19:07:32 +0300

    docs: tasks complited

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -1,9 +1,28 @@
 package subtask1
 
 class HappyArray {
-
-    // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var flag: Boolean = true
+        var inputArray = sadArray
+
+        if ( sadArray.isEmpty() ) {
+            return sadArray
+        }
+        while (flag) {
+            val happyArray = mutableListOf<Int>()
+            flag = false
+
+            happyArray.add(inputArray.first())
+            for ( i in 1 until inputArray.lastIndex) {
+                if ( inputArray[i] < inputArray[i-1] + inputArray[i+1] ) {
+                    happyArray.add(inputArray[i])
+                } else {
+                    flag = true
+                }
+            }
+            happyArray.add(inputArray.last())
+            inputArray = happyArray.toIntArray();
+        }
+        return inputArray
     }
 }
--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -1,9 +1,50 @@
 package subtask3
 
 class StringParser {
-
-    // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val result = mutableListOf<String>()
+
+        var startSubstr: Int
+        var endSubstr: Int
+        var substr: String
+        var hasSubstr = false
+
+        for ( i in 0..inputString.lastIndex ) {
+            if ( inputString[i] == '[' || inputString[i] == '<' || inputString[i] == '(' ) {
+                startSubstr = i
+                for ( j in inputString.lastIndex downTo i ) {
+                    if ( when {
+                        inputString[startSubstr] == '[' && inputString[j] == ']' -> true
+                            inputString[startSubstr] == '<' && inputString[j] == '>' -> true
+                            inputString[startSubstr] == '(' && inputString[j] == ')' -> true
+                            else -> false
+                    } ) {
+                        endSubstr = j
+                        for ( k in j-1 downTo i+1 ) {
+                            if ( when {
+                                inputString[startSubstr] == '[' && inputString[k] == ']' -> true
+                                    inputString[startSubstr] == '<' && inputString[k] == '>' -> true
+                                    inputString[startSubstr] == '(' && inputString[k] == ')' -> true
+                                    else -> false
+                            } ) {
+                                for ( m in i+1..k ) {
+                                    if ( inputString[m] == '[' || inputString[m] == '<' || inputString[m] == '(' ) {
+                                        hasSubstr = true
+                                    }
+                                }
+                                if ( !hasSubstr ) {
+                                    endSubstr = k
+                                }
+                            }
+                        }
+                        substr = inputString.substring(startSubstr+1, endSubstr)
+                        if ( substr !in result ) {
+                            result.add(substr)
+                        }
+                    }
+                }
+            }
+        }
+        return result.toTypedArray()
     }
 }
